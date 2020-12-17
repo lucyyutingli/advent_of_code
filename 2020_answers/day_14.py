245542362e361e00598fa6cae67c2f4afc69042b
Lucy Li <47697986+lucyyutingli@users.noreply.github.com>
2020-12-17 01:14:33 -0800

    finished day 14

--- 2020_answers/day_14.py
+++ 2020_answers/day_14.py
@@ -1,5 +1,6 @@
 import re
 from collections import defaultdict
+from itertools import product
 
 file = open("day14_input.txt", "r")
 full_input = file.read().split('\n')
@@ -21,33 +22,44 @@ def clean_groups(array):
     mask = array[0].split()[2]
     memory_num = []        
     for i in array[1:]:
-        memory_id = re.search(r'\[(.+?)\]', i).group(1)
+        memory_id = int(re.search(r'\[(.+?)\]', i).group(1))
         memory_input = int(i.split(" ")[2])
         memory_num.append((memory_id, memory_input))
         
     return mask, memory_num
 
-
+global mem
+mem = defaultdict()
 def masked_memories(mask, array):
-    mem = defaultdict()
+    global mem
     for i in array:
         new_bit = ""
-        original_bit ="{0:036b}".format(i[1])
+        original_mem ="{0:036b}".format(i[0])
         for j,k in enumerate(mask):
             if k == "X":
-                new_bit += original_bit[j]
+                new_bit += "X"
             elif k == "1":
                 new_bit += "1"
             elif k == "0":
-                new_bit += "0"
-        mem[i[0]] = int(new_bit,2)
-    return sum(mem.values())
-
+                new_bit +=  original_mem[j]
+    
+        x_count = new_bit.count("X")
+        possible_combos = [p for p in product([0,1], repeat = x_count)]
+        
+        for l in possible_combos:
+            copy_bit = new_bit
+            for j in l:
+                copy_bit = copy_bit.replace("X", str(j), 1)
+            mem[int(copy_bit,2)] = i[1]
+            
+            
+            
 def complete_sum(array):
     total = 0
     for i in array:
         mask, memory_list = clean_groups(i)
-        total += masked_memories(mask, memory_list)
+        masked_memories(mask, memory_list)
+    total = sum(mem.values())
     print(total)
     
 complete_sum(mask_groups)
